1c2efd8033aff0091aa74be323602de8997466ad
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-09-23 15:48:41 +0900

    빈 BMP 출력

--- renderer/build.sh
+++ renderer/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-g++ main.cpp -orenderer --std=c++17 \
+g++ -g \
+main.cpp \
+bmp.cpp \
+loader.cpp \
+error.cpp \
+-orenderer --std=c++17 \
 $(pkg-config --cflags --libs gl) \
 $(pkg-config --cflags --libs osmesa) \
-$(pkg-config --cflags --libs glu)
+$(pkg-config --cflags --libs glu) \
+$(pkg-config --cflags --libs glew)
--- renderer/main.cpp
+++ renderer/main.cpp
@@ -1,7 +1,56 @@
+#include <string>
+
 #include <GL/gl.h>
 #include <GL/osmesa.h>
 
-int main()
+#include "renderer.h"
+
+int viewportWidth, viewportHeight;
+std::string inputFilePath;
+std::string outputFilePath;
+
+void parseArgs(int argc, char *argv[])
 {
+    if (argc != 5)
+    {
+        error("parseArgs");
+    }
+
+    viewportWidth = std::stoi(argv[1]);
+    viewportHeight = std::stoi(argv[2]);
+    inputFilePath = std::string(argv[3]);
+    outputFilePath = std::string(argv[4]);
+}
+
+int main(int argc, char *argv[])
+{
+    OSMesaContext ctx = OSMesaCreateContextExt(OSMESA_RGBA, 24, 8, 8, NULL);
+    if (!ctx)
+    {
+        error("OSMesaCreateContextExt");
+    }
+
+    parseArgs(argc, argv);
+
+    char *buffer = (char *)malloc(viewportWidth * viewportHeight * 4 * sizeof(GLubyte));
+    if (!buffer)
+    {
+        error("buffer");
+    }
+
+    if (!OSMesaMakeCurrent(ctx, buffer, GL_UNSIGNED_BYTE, viewportWidth, viewportHeight))
+    {
+        error("OSMesaMakeCurrent");
+    }
+
+    fprintf(stderr, "GL_VERSION %s\n", glGetString(GL_VERSION));
+
+    glFinish();
+
+    writeBMP(outputFilePath.c_str(), viewportWidth, viewportHeight, buffer);
+
+    free(buffer);
+    OSMesaDestroyContext(ctx);
+
     return 0;
 }
