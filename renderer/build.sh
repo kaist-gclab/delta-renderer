91bbe6d3233bd455b4dcbb223be70de123107725
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:53:34 +0900

    빌드 스크립트 Format

--- renderer/build.sh
+++ renderer/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 g++ -g -Wall \
--O2 \
-main.cpp \
-bmp.cpp \
-gmodel.cpp \
-error.cpp \
-grenderer.cpp \
-util.cpp \
-png.cpp \
--orenderer --std=c++17 \
-$(pkg-config --cflags --libs gl) \
-$(pkg-config --cflags --libs osmesa) \
--lpng
+    -O2 \
+    main.cpp \
+    bmp.cpp \
+    gmodel.cpp \
+    error.cpp \
+    grenderer.cpp \
+    util.cpp \
+    png.cpp \
+    -orenderer --std=c++17 \
+    $(pkg-config --cflags --libs gl) \
+    $(pkg-config --cflags --libs osmesa) \
+    -lpng
