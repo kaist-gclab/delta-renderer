665dc9e0a86fc4e72f72847635ff2b7c4e2bfd97
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 11:57:29 +0900

    다중 형식 지원 준비

--- renderer/renderer.h
+++ renderer/renderer.h
@@ -6,3 +6,6 @@ void error(std::string_view message);
 
 void writeBMP(FILE *out, int width, int height, const char *bufferRgba);
 void writePNG(FILE *out, int width, int height, const char *bufferRgba);
+
+const int FORMAT_BMP = 1;
+const int FORMAT_PNG = 2;
