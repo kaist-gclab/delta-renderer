f0f70a7eeaee073231600b69818bc01714fb2ccd
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 20:04:53 +0900

    writeBMP 인터페이스 변경

--- renderer/renderer.h
+++ renderer/renderer.h
@@ -4,7 +4,7 @@
 
 void error(std::string_view message);
 
-void writeBMP(FILE *out, int width, int height, const char *bufferRgba);
+void writeBMP(const char *outputFilePath, int width, int height, const char *bufferRgba);
 void writePNG(const char *outputFilePath, int width, int height, const char *bufferRgba);
 
 const int FORMAT_BMP = 1;
